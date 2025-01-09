6f737b50eddde94af45bc80aaa4024d5c7e80fca
Moritz Gmeiner <moritz.gmeiner@fau.de>
2025-01-09 20:50:29 +0100

    keep original dataset in DegenerateDataset

--- hyper_lap/datasets/degenerate.py
+++ hyper_lap/datasets/degenerate.py
@@ -1,15 +1,21 @@
 import numpy as np
-
 from torch.utils.data import Dataset
 
 
 class DegenerateDataset(Dataset):
+    len: int
+    item: dict[str, np.ndarray]
+
+    orig_dataset: Dataset
+
     def __init__(self, dataset):
         super().__init__()
 
         self.len = len(dataset)
 
         self.item = dataset[0]
+
+        self.orig_dataset = dataset
 
     def __len__(self) -> int:
         return self.len
