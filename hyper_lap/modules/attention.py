48e9dbbedf976e0282ecdf54b75581e38fe82ded
Moritz Gmeiner <moritz.gmeiner@fau.de>
2025-02-17 13:59:03 +0100

    added attention Unet

--- hyper_lap/modules/attention.py
+++ hyper_lap/modules/attention.py
@@ -1,10 +1,11 @@
 from jaxtyping import Array, Float, PRNGKeyArray
+from typing import Optional
 
 import equinox as eqx
 import equinox.nn as nn
 import jax
 import jax.random as jr
-from chex import assert_rank, assert_shape
+from chex import assert_axis_dimension, assert_equal_shape, assert_rank, assert_shape
 
 
 class FeedForward(eqx.Module):
@@ -69,20 +70,30 @@ class Attention(eqx.Module):
 
         assert hidden_dim == self.num_heads * self.dim_head
 
-        return x.reshape(n_seq, self.num_heads, self.dim_head).transpose(1, 0, 2)
+        return x.reshape(n_seq, self.num_heads, self.dim_head)  # .transpose(1, 0, 2)
 
-    def __call__(self, x: Float[Array, "n d"]) -> Float[Array, "n d"]:
+    def __call__(
+        self, x: Float[Array, "n d"], context: Optional[Float[Array, "m d"]] = None
+    ) -> Float[Array, "n d"]:
         assert_rank(x, 2)
-        assert x.shape[1] == self.dim_model
+        assert_axis_dimension(x, 1, self.dim_model)
+
+        if context is None:
+            context = x
+
+        assert_rank(context, 2)
+        assert_axis_dimension(context, 1, self.dim_model)
 
         n_seq = x.shape[0]
+        m_seq = context.shape[0]
 
-        # shape: [num_heads, context, dim_head]
+        # shape: [num_heads, context, dim_head]1
         q = self.transpose_for_scores(jax.vmap(self.query)(x))
-        k = self.transpose_for_scores(jax.vmap(self.key)(x))
-        v = self.transpose_for_scores(jax.vmap(self.value)(x))
+        k = self.transpose_for_scores(jax.vmap(self.key)(context))
+        v = self.transpose_for_scores(jax.vmap(self.value)(context))
 
-        assert_shape([q, k, v], [self.num_heads, n_seq, self.dim_head])
+        assert_shape(q, [n_seq, self.num_heads, self.dim_head])
+        assert_shape([k, v], [m_seq, self.num_heads, self.dim_head])
 
         # # shape: [num_heads, context, context]
         # qk = self.scale_factor * (q @ k.mT)
@@ -105,69 +116,14 @@ class Attention(eqx.Module):
 
         out = jax.nn.dot_product_attention(q, k, v)
 
-        assert_shape(out, [self.num_heads, n_seq, self.dim_head])
+        assert_shape(out, [n_seq, self.num_heads, self.dim_head])
 
-        out = out.swapaxes(0, 1).reshape(n_seq, self.num_heads * self.dim_head)
+        # squash heads back to hidden_dim
+        out = out.reshape(n_seq, self.num_heads * self.dim_head)
 
         out = jax.vmap(self.out_proj)(out)
 
         assert_shape(out, [n_seq, self.dim_model])
-
-        return out
-
-
-class SpatialAttention(eqx.Module):
-    num_heads: int = eqx.field(static=True)
-    dim_head: int = eqx.field(static=True)
-
-    scale_factor: float
-
-    def __init__(self, channels: int, num_heads: int, dim_head: int, *, key: PRNGKeyArray):
-        super().__init__()
-
-        self.num_heads = num_heads
-        self.dim_head = dim_head
-
-        hidden_dim = num_heads * dim_head
-
-        qkv_key, out_key = jr.split(key)
-
-        self.qkv = nn.Conv2d(channels, 3 * hidden_dim, kernel_size=1, use_bias=False, key=qkv_key)
-
-        self.out_proj = nn.Conv2d(hidden_dim, channels, kernel_size=1, key=out_key)
-
-    def __call__(self, x: Float[Array, "c h w"]) -> Float[Array, "c h w"]:
-        c, h, w = x.shape
-
-        # shape: [3, num_heads, hw, dim_head]
-        qkv = self.qkv(x).reshape(3, self.num_heads, self.dim_head, h * w).swapaxes(2, 3)
-
-        assert_shape(qkv, [3, self.num_heads, h * w, self.dim_head])
-
-        q, k, v = qkv[:]
-
-        # # all of shape [num_heads, hw, dim_head]
-        # q, k, v = qkv[:]
-        #
-        # # of shape [num_heads, hw, hw]
-        # qk = self.scale_factor * (q @ k.mT)
-        #
-        # attn = jax.nn.softmax(qk, axis=-1)
-        #
-        # # shape: [num_heads, hw, dim_head]
-        # out = attn @ v
-        #
-        # out = out.transpose(0, 2, 1).reshape(-1, h, w)
-
-        assert_shape([q, k, v], [self.num_heads, h * w, self.dim_head])
-
-        out = jax.nn.dot_product_attention(q, k, v)
-
-        assert_shape(out, [self.num_heads, h * w, self.dim_head])
-
-        out = out.transpose(0, 2, 1).reshape(-1, h, w)
-
-        out = self.out_proj(out)
 
         return out
 
@@ -228,3 +184,91 @@ class Encoder(eqx.Module):
             x = res_attn_block(x)
 
         return x
+
+
+class SpatialSelfAttention(eqx.Module):
+    num_heads: int = eqx.field(static=True)
+    dim_head: int = eqx.field(static=True)
+
+    scale_factor: float = eqx.field(static=True)
+
+    qkv: nn.Conv2d
+
+    def __init__(self, channels: int, num_heads: int, dim_head: int, *, key: PRNGKeyArray):
+        super().__init__()
+
+        self.num_heads = num_heads
+        self.dim_head = dim_head
+
+        hidden_dim = num_heads * dim_head
+
+        qkv_key, out_key = jr.split(key)
+
+        self.qkv = nn.Conv2d(channels, 3 * hidden_dim, kernel_size=1, use_bias=False, key=qkv_key)
+
+        self.out_proj = nn.Conv2d(hidden_dim, channels, kernel_size=1, key=out_key)
+
+    def __call__(self, x: Float[Array, "c h w"]) -> Float[Array, "c h w"]:
+        c, h, w = x.shape
+
+        # shape: [3, num_heads, hw, dim_head]
+        qkv = self.qkv(x).reshape(3, self.num_heads, self.dim_head, h * w).transpose(0, 3, 1, 2)
+
+        assert_shape(qkv, [3, h * w, self.num_heads, self.dim_head])
+
+        q, k, v = qkv[:]
+
+        # # all of shape [num_heads, hw, dim_head]
+        # q, k, v = qkv[:]
+        #
+        # # of shape [num_heads, hw, hw]
+        # qk = self.scale_factor * (q @ k.mT)
+        #
+        # attn = jax.nn.softmax(qk, axis=-1)
+        #
+        # # shape: [num_heads, hw, dim_head]
+        # out = attn @ v
+        #
+        # out = out.transpose(0, 2, 1).reshape(-1, h, w)
+
+        assert_shape([q, k, v], [h * w, self.num_heads, self.dim_head])
+
+        out = jax.nn.dot_product_attention(q, k, v)
+
+        assert_shape(out, [h * w, self.num_heads, self.dim_head])
+
+        out = out.transpose(1, 2, 0).reshape(-1, h, w)
+
+        out = self.out_proj(out)
+
+        return out
+
+
+class SpatialCrossAttention(eqx.Module):
+    """
+    Attention block that enables spatial cross-attention between an image and a context.
+    """
+
+    attn: Attention
+
+    def __init__(self, channels: int, num_heads: int, dim_head: int, *, key: PRNGKeyArray):
+        super().__init__()
+
+        self.attn = Attention(channels, num_heads, dim_head, key=key)
+
+    def __call__(
+        self, img: Float[Array, "c h w"], context: Float[Array, "m d"]
+    ) -> Float[Array, "c h w"]:
+        c, h, w = img.shape
+
+        x = img.reshape(c, h * w).transpose(1, 0)
+
+        assert_equal_shape([x, context], dims=1)
+
+        out = self.attn(x, context)
+
+        out = out.transpose(1, 0).reshape(c, h, w)
+
+        assert_equal_shape([img, out])
+
+        return out
---
+++ hyper_lap/modules/attn_unet.py
@@ -0,0 +1,284 @@
+from jaxtyping import Array, Float, PRNGKeyArray
+from typing import Any, Optional, Sequence
+
+import equinox as eqx
+import equinox.nn as nn
+import jax.numpy as jnp
+import jax.random as jr
+
+from .attention import SpatialCrossAttention
+from .conv import ConvNormAct
+from .upsample import BilinearUpsample2d
+
+
+class ResBlock(eqx.Module):
+    res_conv: Optional[nn.Conv2d]
+
+    use_res: bool = eqx.field(static=True)
+
+    layers: nn.Sequential
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = 3,
+        groups: int = 8,
+        n_convs: int = 2,
+        use_res: bool = False,
+        *,
+        use_weight_standardized_conv: bool,
+        key: PRNGKeyArray,
+    ):
+        super().__init__()
+
+        if n_convs < 1:
+            raise ValueError("Must have at least one conv")
+
+        self.use_res = use_res
+
+        if use_res and in_channels != out_channels:
+            key, consume = jr.split(key)
+
+            self.res_conv = nn.Conv2d(in_channels, out_channels, 1, padding="SAME", key=consume)
+        else:
+            self.res_conv = None
+
+        keys = jr.split(key, n_convs)
+
+        layers = [
+            ConvNormAct(
+                in_channels,
+                out_channels,
+                kernel_size,
+                groups=groups,
+                use_weight_standardized_conv=use_weight_standardized_conv,
+                key=keys[0],
+            )
+        ]
+
+        layers += [
+            ConvNormAct(
+                out_channels,
+                out_channels,
+                kernel_size,
+                groups=groups,
+                use_weight_standardized_conv=use_weight_standardized_conv,
+                key=key,
+            )
+            for key in keys[1:]
+        ]
+
+        self.layers = nn.Sequential(layers)
+
+    def __call__(
+        self, x: Float[Array, "c h w d"], *, key: Optional[PRNGKeyArray] = None
+    ) -> Float[Array, "c h w d"]:
+        res = x
+
+        for layer in self.layers:
+            x = layer(x)
+
+        if self.use_res:
+            if self.res_conv is not None:
+                res = self.res_conv(res)
+
+            x += res
+
+        return x
+
+
+class AttnUnetDown(eqx.Module):
+    base_channels: int = eqx.field(static=True)
+    channel_mults: list[int] = eqx.field(static=True)
+
+    res_blocks: list[ResBlock]
+    attn_blocks: list[SpatialCrossAttention]
+    downs: list[nn.MaxPool2d]
+
+    def __init__(
+        self,
+        base_channels: int,
+        channel_mults: Sequence[int],
+        *,
+        key: PRNGKeyArray,
+        block_args: Optional[dict[str, Any]] = None,
+    ):
+        super().__init__()
+
+        if block_args is None:
+            block_args = {}
+
+        self.base_channels = base_channels
+        self.channel_mults = list(channel_mults)
+
+        channels = base_channels
+
+        self.res_blocks = []
+        self.downs = []
+
+        for channel_mult in channel_mults[1:]:
+            new_channels = channel_mult * base_channels
+
+            key, res_key, attn_key = jr.split(key, 3)
+
+            self.res_blocks.append(ResBlock(channels, new_channels, key=res_key, **block_args))
+
+            channels = new_channels
+
+            self.attn_blocks.append(SpatialCrossAttention(channels, 4, channels // 2, key=attn_key))
+
+            self.downs.append(nn.MaxPool2d(2, 2))
+
+    def __call__(
+        self, x: Array, context: Array, *, key: Optional[PRNGKeyArray] = None
+    ) -> tuple[Array, list[Array]]:
+        skips: list[Array] = []
+
+        for res_block, attn_block, down in zip(self.res_blocks, self.attn_blocks, self.downs):
+            x = res_block(x)
+
+            x = attn_block(x, context)
+
+            skips.append(x)
+
+            c, h, w = x.shape
+
+            assert h % 2 == 0 and w % 2 == 0, (
+                f"spatial dims of shape {x.shape} are not divisible by 2"
+            )
+
+            x = down(x)
+
+        return x, skips
+
+
+class AttnUnetUp(eqx.Module):
+    base_channels: int = eqx.field(static=True)
+    channel_mults: list[int] = eqx.field(static=True)
+
+    res_blocks: list[ResBlock]
+    attn_blocks: list[SpatialCrossAttention]
+    ups: list[BilinearUpsample2d]
+
+    def __init__(
+        self,
+        base_channels: int,
+        channel_mults: Sequence[int],
+        *,
+        key: PRNGKeyArray,
+        block_args: Optional[dict[str, Any]] = None,
+    ):
+        super().__init__()
+
+        if block_args is None:
+            block_args = {}
+
+        self.base_channels = base_channels
+        self.channel_mults = list(channel_mults)
+
+        self.res_blocks = []
+        self.ups = []
+
+        channels = base_channels * channel_mults[-1]
+
+        for channel_mult in list(reversed(channel_mults))[1:]:
+            new_channels = channel_mult * base_channels
+
+            key, res_key, attn_key = jr.split(key, 3)
+
+            # self.ups.append(ConvUpsample2d(channels, channels, key=up_key))
+            self.ups.append(BilinearUpsample2d())
+
+            self.res_blocks.append(ResBlock(2 * channels, new_channels, key=res_key, **block_args))
+
+            self.attn_blocks.append(SpatialCrossAttention(channels, 4, channels // 2, key=attn_key))
+
+            channels = new_channels
+
+    def __call__(
+        self, x: Array, context: Array, skips: list[Array], *, key: Optional[PRNGKeyArray] = None
+    ) -> Array:
+        skips = skips.copy()
+
+        for up, res_block, attn_block in zip(self.ups, self.res_blocks, self.attn_blocks):
+            x = up(x)
+
+            skip = skips.pop()
+
+            x = jnp.concat([skip, x], axis=0)
+
+            x = res_block(x)
+
+            x = attn_block(x, context)
+
+        return x
+
+
+class AttnUnetModule(eqx.Module):
+    base_channels: int = eqx.field(static=True)
+    channel_mults: list[int] = eqx.field(static=True)
+
+    down: AttnUnetDown
+
+    middle_res1: ResBlock
+    middle_attn: SpatialCrossAttention
+    middle_res2: ResBlock
+
+    up: AttnUnetUp
+
+    def __init__(
+        self,
+        base_channels: int,
+        channel_mults: Sequence[int],
+        *,
+        key: PRNGKeyArray,
+        block_args: Optional[dict[str, Any]] = None,
+    ):
+        super().__init__()
+
+        self.base_channels = base_channels
+        self.channel_mults = list(channel_mults)
+
+        down_key, middle_key, up_key = jr.split(key, 3)
+
+        self.down = AttnUnetDown(base_channels, channel_mults, key=down_key, block_args=block_args)
+
+        middle_channels = base_channels * channel_mults[-1]
+
+        res1_key, attn_key, res2_key = jr.split(middle_key, 3)
+
+        self.middle_res1 = ResBlock(
+            middle_channels, middle_channels, key=res1_key, **(block_args or {})
+        )
+        self.middle_attn = SpatialCrossAttention(
+            middle_channels, 4, middle_channels // 2, key=attn_key
+        )
+        self.middle_res2 = ResBlock(
+            middle_channels, middle_channels, key=res2_key, **(block_args or {})
+        )
+
+        self.up = AttnUnetUp(base_channels, channel_mults, key=up_key, block_args=block_args)
+
+    def __call__(self, x: Array, context: Array, *, key: Optional[PRNGKeyArray] = None) -> Array:
+        c, h, w = x.shape
+
+        down_factor = 2 ** len(self.channel_mults)
+
+        assert h % down_factor == 0, (
+            f"spatial dims must be divisible by {down_factor}, but shape is {x.shape}"
+        )
+        assert w % down_factor == 0, (
+            f"spatial dims must be divisible by {down_factor}, but shape is {x.shape}"
+        )
+
+        x, skips = self.down(x, context)
+
+        # x = self.middle(x)
+        x = self.middle_res1(x)
+        x = self.middle_attn(x, context)
+        x = self.middle_res2(x)
+
+        x = self.up(x, context, skips)
+
+        return x
