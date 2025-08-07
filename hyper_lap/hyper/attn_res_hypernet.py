cf3bca11c2cb480224d76eed42c86085987033cd
Moritz Gmeiner <moritz.gmeiner@fau.de>
2025-08-08 00:25:42 +0200

    added AttnResHyperNet with training scripts
    also renamed train_hypernet_res.py to train_res_hypernet.py

--- hyper_lap/hyper/attn_res_hypernet.py
+++ hyper_lap/hyper/attn_res_hypernet.py
@@ -1,4 +1,4 @@
-from jaxtyping import Array, Float, Integer, PRNGKeyArray, PyTree, Scalar
+from jaxtyping import Array, Float, Integer, PRNGKeyArray, Scalar
 from typing import Any, Literal, overload
 
 import equinox as eqx
@@ -17,24 +17,24 @@ from hyper_lap.hyper.generator import (
     Conv2dLoraGenerator,
 )
 from hyper_lap.models import Unet
-from hyper_lap.modules.unet import ConvNormAct, ResBlock, UnetModule
+from hyper_lap.modules.attention import Encoder
+from hyper_lap.modules.unet import ConvNormAct, UnetModule
 
 
-class ResHyperNet(eqx.Module):
-    unet: Unet  # = eqx.field(static=True)
-
-    filter_spec: PyTree = eqx.field(static=True)
+class AttnResHyperNet(eqx.Module):
+    unet: Unet  #  = eqx.field(static=True)
 
     kernel_size: int = eqx.field(static=True)
     base_channels: int = eqx.field(static=True)
 
     block_size: int = eqx.field(static=True)
 
-    input_emb_size: int = eqx.field(static=True)
-    pos_emb_size: int = eqx.field(static=True)
+    emb_size: int = eqx.field(static=True)
 
     kernel_generator: Conv2dGeneratorABC
     resample_generator: Conv2dGeneratorABC
+
+    transformer: Encoder
 
     unet_pos_embs: list[Array]
     recomb_pos_embs: list[Array]
@@ -43,75 +43,71 @@ class ResHyperNet(eqx.Module):
     final_kernel: Array
 
     @staticmethod
-    def init_conv_generator(
-        gen: Conv2dGeneratorABC, eps: float, *, key: PRNGKeyArray
-    ) -> Conv2dGeneratorABC:
-        def init_linear(linear: nn.Linear, *, key: PRNGKeyArray) -> nn.Linear:
-            weight_key, bias_key = jr.split(key)
+    def flatten_with_def(pos_embs: list[Array]) -> tuple[list[Array], list[tuple[int, int]]]:
+        out = []
+        out_shapes = []
 
-            linear = eqx.tree_at(
-                lambda linear: linear.weight,
-                linear,
-                eps * jr.normal(weight_key, linear.weight.shape),
-            )
+        for pos_emb in pos_embs:
+            n, m, _ = pos_emb.shape
 
-            if linear.use_bias:
-                assert linear.bias is not None
+            out_shapes.append((n, m))
 
-                linear = eqx.tree_at(
-                    lambda linear: linear.bias,
-                    linear,
-                    eps * jr.normal(bias_key, linear.bias.shape),
-                )
+            pos_emb = pos_emb.reshape(n * m, -1)
 
-            return linear
+            out += list(pos_emb)
 
-        linears, treedef = jt.flatten(gen, is_leaf=lambda x: isinstance(x, eqx.nn.Linear))
+        assert_equal_shape(out)
 
-        keys = jr.split(key, len(linears))
+        return out, out_shapes
 
-        linears = [init_linear(linear, key=key) for linear, key in zip(linears, keys)]
+    @staticmethod
+    def unflatten_from_def(
+        flat_pos_embs: list[Array], shapes: list[tuple[int, int]]
+    ) -> list[Array]:
+        out = []
 
-        gen = jt.unflatten(treedef, linears)
+        assert len(flat_pos_embs) == sum(n * m for (n, m) in shapes), (
+            f"{len(flat_pos_embs)=}, but {sum(n * m for (n, m) in shapes)=}"
+        )
 
-        return gen
+        for n, m in shapes:
+            assert len(flat_pos_embs) >= n * m
+
+            flat_pos_emb = flat_pos_embs[: n * m]
+
+            flat_pos_embs = flat_pos_embs[n * m :]
+
+            pos_emb = jnp.stack(flat_pos_emb)
+
+            pos_emb = pos_emb.reshape(n, m, -1)
+
+            out.append(pos_emb)
+
+        return out
 
     def __init__(
         self,
         unet: Unet,
         block_size: int,
-        input_emb_size: int,
-        pos_emb_size: int,
+        emb_size: int,
         *,
+        transformer_depth: int,
         kernel_size: int = 3,
-        filter_spec: PyTree | None = None,
-        generator_kind: Literal["basic", "lora", "new"] = "basic",
+        generator_kind: Literal["basic", "lora"] = "basic",
         generator_kw_args: dict[str, Any] | None = None,
         key: PRNGKeyArray,
     ):
         super().__init__()
 
-        if filter_spec is None:
-            filter_spec = jt.map(lambda x: eqx.is_array(x), unet)
-
-        eps = 1e-5
-
         self.unet = unet
-
-        self.filter_spec = filter_spec
 
         self.kernel_size = kernel_size
         self.base_channels = unet.base_channels
 
         self.block_size = block_size
-        self.input_emb_size = input_emb_size
-        self.pos_emb_size = pos_emb_size
+        self.emb_size = emb_size
 
-        total_emb_size = input_emb_size + pos_emb_size
-
-        base_channels = unet.base_channels
-
-        key, kernel_key, resample_key, emb_key, init_key, final_key = jr.split(key, 6)
+        key, kernel_key, resample_key, init_key, final_key = jr.split(key, 5)
 
         match generator_kind:
             case "basic":
@@ -127,7 +123,7 @@ class ResHyperNet(eqx.Module):
             block_size,
             block_size,
             kernel_size,
-            total_emb_size,
+            emb_size,
             key=kernel_key,
             **(generator_kw_args or {}),
         )
@@ -136,33 +132,32 @@ class ResHyperNet(eqx.Module):
             block_size,
             block_size,
             1,
-            pos_emb_size,
+            emb_size,
             key=resample_key,
             **(generator_kw_args or {}),
         )
 
-        # we can reuse kernel_key here since we re-initialize the weights here
-        self.kernel_generator = self.init_conv_generator(self.kernel_generator, eps, key=kernel_key)
-
-        self.init_kernel = eps * jr.normal(init_key, unet.init_conv.conv.weight.shape)
-        self.final_kernel = eps * jr.normal(final_key, unet.final_conv.weight.shape)
+        self.init_kernel = jr.normal(init_key, unet.init_conv.conv.weight.shape)
+        self.final_kernel = jr.normal(final_key, unet.final_conv.weight.shape)
 
         # generate positional embeddings for unet module
 
-        unet_pos_embs_key, recomb_pos_embs_key = jr.split(key)
+        transformer_key, unet_pos_embs_key, recomb_pos_embs_key = jr.split(key, 3)
 
-        self.unet_pos_embs = self.generate_pos_embs(
-            unet.unet, self.filter_spec.unet, key=unet_pos_embs_key
+        self.transformer = Encoder(
+            depth=transformer_depth,
+            d_model=emb_size,
+            num_heads=emb_size // 64,
+            d_head=64,
+            key=transformer_key,
         )
 
-        self.recomb_pos_embs = self.generate_pos_embs(
-            unet.recomb, self.filter_spec.recomb, key=recomb_pos_embs_key
-        )
+        self.unet_pos_embs = self.generate_pos_embs(unet.unet, key=unet_pos_embs_key)
 
-    def generate_pos_embs(
-        self, module: eqx.Module, filter_spec: PyTree, *, key: PRNGKeyArray
-    ) -> list[Array]:
-        leaves, _ = jt.flatten(eqx.filter(module, filter_spec))
+        self.recomb_pos_embs = self.generate_pos_embs(unet.recomb, key=recomb_pos_embs_key)
+
+    def generate_pos_embs(self, module: eqx.Module, *, key: PRNGKeyArray) -> list[Array]:
+        leaves, _ = jt.flatten(eqx.filter(module, eqx.is_array))
 
         block_size = self.block_size
         kernel_size = self.kernel_size
@@ -186,7 +181,7 @@ class ResHyperNet(eqx.Module):
 
             key, consume = jr.split(key)
 
-            emb = jr.normal(consume, [b_out, b_in, self.pos_emb_size])
+            emb = jr.normal(consume, [b_out, b_in, self.emb_size])
 
             embs.append(emb)
 
@@ -194,54 +189,32 @@ class ResHyperNet(eqx.Module):
 
     def gen_init_conv(self, init_conv: ConvNormAct) -> ConvNormAct:
         assert_equal_shape([init_conv.conv.weight, self.init_kernel])
-
         init_conv = eqx.tree_at(
-            lambda _init_conv: _init_conv.conv.weight,
-            init_conv,
-            init_conv.conv.weight + self.init_kernel,
+            lambda _init_conv: _init_conv.conv.weight, init_conv, self.init_kernel
         )
-
-        reg = (self.init_kernel**2).sum()
 
         return init_conv
 
     def gen_final_conv(self, final_conv: nn.Conv2d) -> nn.Conv2d:
         assert_equal_shape([final_conv.weight, self.final_kernel])
-
         final_conv = eqx.tree_at(
-            lambda _final_conv: _final_conv.weight,
-            final_conv,
-            final_conv.weight + self.final_kernel,
+            lambda _final_conv: _final_conv.weight, final_conv, self.final_kernel
         )
 
         return final_conv
 
-    def gen_weights(
-        self,
-        unet: UnetModule,
-        input_emb: Array,
-        pos_embs: list[Array],
-        filter_spec: PyTree | None = None,
-    ) -> tuple[UnetModule, Scalar]:
-        if filter_spec is None:
-            filter_spec = eqx.is_array
-
-        model_weights, static_model = eqx.partition(unet, filter_spec)
+    def gen_weights(self, unet: UnetModule, embs: list[Array]) -> tuple[UnetModule, Scalar]:
+        model_weights, static_model = eqx.partition(unet, eqx.is_array)
 
         weights, treedef = jt.flatten(model_weights)
 
-        assert len(weights) == len(pos_embs), (
-            f"expected {len(pos_embs)} weights, found {len(weights)} instead"
+        assert len(weights) == len(embs), (
+            f"expected {len(embs)} weights, found {len(weights)} instead"
         )
 
         # vmap over block in positional embeddings
 
-        # input_emb is same for all weights, so we can capture it
-        def kernel_generator(pos_emb):
-            emb = jnp.concat([input_emb, pos_emb])
-
-            return self.kernel_generator(emb)
-
+        kernel_generator = self.kernel_generator
         kernel_generator = jax.vmap(kernel_generator)
         kernel_generator = jax.vmap(kernel_generator)
 
@@ -251,8 +224,8 @@ class ResHyperNet(eqx.Module):
 
         reg = jnp.array(0.0)
 
-        for i, pos_emb in enumerate(pos_embs):
-            b_out, b_in, _ = pos_emb.shape
+        for i, emb in enumerate(embs):
+            b_out, b_in, _ = emb.shape
 
             c_out, c_in, k1, k2 = weights[i].shape
 
@@ -261,9 +234,9 @@ class ResHyperNet(eqx.Module):
             assert b_in == c_in // self.block_size
 
             if k1 == self.kernel_size:
-                weight = kernel_generator(pos_emb)
+                weight = kernel_generator(emb)
             elif k1 == 1:
-                weight = resample_generator(pos_emb)
+                weight = resample_generator(emb)
             else:
                 raise RuntimeError(f"weight has unexpected shape {weights[i].shape}")
 
@@ -301,13 +274,8 @@ class ResHyperNet(eqx.Module):
     ) -> Unet: ...
 
     def generate(
-        self,
-        input_emb: Array,
-        *,
-        with_aux: bool = False,
+        self, input_emb: Array, with_aux: bool = False
     ) -> Unet | tuple[Unet, dict[str, Any]]:
-        aux = {}
-
         dyn_unet, static_unet = eqx.partition(self.unet, eqx.is_array)
 
         dyn_unet = jax.lax.stop_gradient(dyn_unet)
@@ -320,12 +288,38 @@ class ResHyperNet(eqx.Module):
         init_conv = self.gen_init_conv(init_conv)
         final_conv = self.gen_final_conv(final_conv)
 
-        unet, unet_reg = self.gen_weights(
-            unet, input_emb, self.unet_pos_embs, self.filter_spec.unet
+        unet_pos_embs = self.unet_pos_embs
+        recomb_pos_embs = self.recomb_pos_embs
+
+        unet_pos_embs_flat, unet_pos_embs_shapes = AttnResHyperNet.flatten_with_def(unet_pos_embs)
+        recomb_pos_embs_flat, recomb_pos_embs_shapes = AttnResHyperNet.flatten_with_def(
+            recomb_pos_embs
         )
-        recomb, recomb_reg = self.gen_weights(
-            recomb, input_emb, self.recomb_pos_embs, self.filter_spec.recomb
+
+        all_embs_list = [input_emb] + unet_pos_embs_flat + recomb_pos_embs_flat
+        assert_shape(all_embs_list, (self.emb_size,))
+
+        all_embs = jnp.stack(all_embs_list, axis=0)
+
+        all_embs = self.transformer(all_embs)
+
+        unet_embs_flat = list(all_embs[1 : len(unet_pos_embs_flat) + 1])
+        recomb_embs_flat = list(all_embs[len(unet_pos_embs_flat) + 1 :])
+
+        unet_embs = AttnResHyperNet.unflatten_from_def(unet_embs_flat, unet_pos_embs_shapes)
+        recomb_embs = AttnResHyperNet.unflatten_from_def(recomb_embs_flat, recomb_pos_embs_shapes)
+
+        assert len(unet_embs) == len(self.unet_pos_embs) and all(
+            unet_emb.shape == unet_pos_emb.shape
+            for unet_emb, unet_pos_emb in zip(unet_embs, self.unet_pos_embs)
         )
+        assert len(recomb_embs) == len(self.recomb_pos_embs) and all(
+            recomb_emb.shape == recomb_pos_emb.shape
+            for recomb_emb, recomb_pos_emb in zip(recomb_embs, self.recomb_pos_embs)
+        )
+
+        unet, unet_reg = self.gen_weights(unet, unet_embs)
+        recomb, recomb_reg = self.gen_weights(recomb, recomb_embs)
 
         dyn_unet = eqx.tree_at(
             lambda _model: (_model.init_conv, _model.unet, _model.recomb, _model.final_conv),
---
+++ scripts/train_res_hypernet.py
@@ -0,0 +1,286 @@
+from jaxtyping import Array
+from typing import Any
+
+from functools import partial
+from pathlib import Path
+
+import equinox as eqx
+import jax
+import jax.random as jr
+import numpy as np
+import optax
+import wandb
+from omegaconf import MISSING, OmegaConf
+from optax import OptState
+from tqdm import tqdm, trange
+
+from hyper_lap.datasets import Dataset
+from hyper_lap.hyper import InputEmbedder, ResHyperNet
+from hyper_lap.models import Unet
+from hyper_lap.serialisation import save_with_config_safetensors
+from hyper_lap.serialisation.safetensors import load_pytree
+from hyper_lap.training.loss import loss_fn
+from hyper_lap.training.trainer import Trainer
+from hyper_lap.training.utils import (
+    load_model_artifact,
+    make_dataloaders,
+    make_lr_schedule,
+    parse_args,
+    print_config,
+)
+
+
+@eqx.filter_jit
+def training_step(
+    hypernet: ResHyperNet,
+    input_embedder: InputEmbedder | None,
+    batch: dict[str, Array],
+    opt: optax.GradientTransformation,
+    opt_state: OptState,
+    *,
+    lamda: float,
+) -> tuple[ResHyperNet, InputEmbedder, OptState, dict[str, Any]]:
+    assert input_embedder is not None
+
+    images = batch["image"]
+    labels = batch["label"]
+    dataset_idx = batch["dataset_idx"]
+
+    def grad_fn(
+        hypernet_embedder: tuple[ResHyperNet, InputEmbedder],
+    ) -> tuple[Array, dict[str, Any]]:
+        hypernet, input_embedder = hypernet_embedder
+
+        input_embedding = input_embedder(images[0], labels[0], dataset_idx)
+
+        model, aux = hypernet.generate(input_embedding, with_aux=True)
+
+        logits = jax.vmap(model)(images)
+
+        ce_loss = jax.vmap(loss_fn)(logits, labels).mean()
+
+        aux["ce_loss"] = ce_loss
+
+        reg_loss = aux["reg_loss"]
+
+        # jax.debug.print("loss={loss}, reg_loss={reg_loss}", loss=loss, reg_loss=reg_loss)
+
+        loss = ce_loss + lamda * reg_loss
+
+        aux["loss"] = loss
+
+        return loss, aux
+
+    (loss, aux), grads = eqx.filter_value_and_grad(grad_fn, has_aux=True)(
+        (hypernet, input_embedder)
+    )
+
+    updates, opt_state = opt.update(grads, opt_state, (hypernet, input_embedder))  # type: ignore
+
+    (hypernet, input_embedder) = eqx.apply_updates((hypernet, input_embedder), updates)
+
+    return hypernet, input_embedder, opt_state, aux  # type: ignore
+
+
+def main():
+    global hypernet
+
+    base_config = OmegaConf.create(
+        {
+            "seed": 42,
+            "dataset": MISSING,
+            "trainsets": MISSING,
+            "testset": MISSING,
+            "degenerate": False,
+            "epochs": MISSING,
+            "lr": MISSING,
+            "batch_size": MISSING,
+            "unet_artifact": "morizzg/idea-laplacian-hypernet/unet-medidec:v73",
+            "lamda": 0.0,
+            "hypernet": {
+                "block_size": 8,
+                "input_emb_size": 1024,
+                "input_emb_size": "${embedder.emb_size}",
+                "pos_emb_size": 1024,
+                "kernel_size": 3,
+                "generator_kind": "basic",
+                "generator_kw_args": {
+                    "h_size": 1024,
+                },
+            },
+            "embedder": {
+                "kind": "clip",
+                "emb_size": 3 * 1024,
+            },
+        }
+    )
+
+    OmegaConf.set_readonly(base_config, True)
+    OmegaConf.set_struct(base_config, True)
+
+    args, arg_config = parse_args()
+
+    if args.wandb:
+        wandb.init(
+            project="idea-laplacian-hypernet",
+            # sync_tensorboard=True,
+        )
+
+    match args.command:
+        case "train":
+            config = OmegaConf.merge(base_config, arg_config)
+
+            if missing_keys := OmegaConf.missing_keys(config):
+                raise RuntimeError(f"Missing mandatory config options: {' '.join(missing_keys)}")
+
+            unet_config, unet_weights_path = load_model_artifact(config.unet_artifact)
+
+            print(f"Loading U-Net weights from {unet_weights_path}")
+
+            unet = Unet(**unet_config["unet"], key=jr.PRNGKey(unet_config["seed"]))  # type: ignore
+
+            unet = load_pytree(unet_weights_path, unet)
+
+            hypernet = ResHyperNet(unet, **config["hypernet"], key=jr.PRNGKey(config["seed"]))  # type: ignore
+
+            first_epoch = unet_config["epochs"]
+
+        case "resume":
+            assert args.artifact is not None
+
+            loaded_config, weights_path = load_model_artifact(args.artifact)
+
+            # TODO: remove this once runs are redone again
+            if "emb_size" in loaded_config["hypernet"]:
+                emb_size = loaded_config["hypernet"].pop("emb_size", None)
+
+                loaded_config["hypernet"]["input_emb_size"] = emb_size
+                loaded_config["hypernet"]["pos_emb_size"] = emb_size
+
+            config = OmegaConf.merge(loaded_config, arg_config)
+
+            if missing_keys := OmegaConf.missing_keys(config):
+                raise RuntimeError(f"Missing mandatory config options: {' '.join(missing_keys)}")
+
+            unet_config, unet_weights_path = load_model_artifact(config.unet_artifact)
+
+            unet = Unet(**unet_config["unet"], key=jr.PRNGKey(unet_config["seed"]))  # type: ignore
+
+            unet = load_pytree(unet_weights_path, unet)
+
+            hypernet = ResHyperNet(unet, **config["hypernet"], key=jr.PRNGKey(config["seed"]))  # type: ignore
+
+            hypernet = load_pytree(weights_path, hypernet)
+
+            first_epoch = unet_config["epochs"] + loaded_config["epochs"]
+
+        case cmd:
+            raise RuntimeError(f"Unrecognised command {cmd}")
+
+    print_config(OmegaConf.to_object(config))
+
+    model_name = f"reshypernet-{config.dataset}-{config.embedder.kind}"
+
+    if wandb.run is not None:
+        wandb.run.name = args.run_name or model_name
+        wandb.run.config.update(OmegaConf.to_object(config))  # type: ignore
+        wandb.run.tags = [config.dataset, config.embedder.kind, "res_hypernet"]
+
+    train_loader, val_loader, test_loader = make_dataloaders(
+        config.dataset,
+        config.trainsets.split(","),
+        config.testset,
+        batch_size=config.batch_size,
+        num_workers=args.num_workers,
+    )
+
+    lr_schedule = make_lr_schedule(config.lr, config.epochs, len(train_loader))
+
+    input_embedder = InputEmbedder(
+        num_datasets=len(train_loader.datasets),
+        **config.embedder,
+        key=jr.PRNGKey(config.seed),
+    )
+
+    trainer: Trainer[ResHyperNet] = Trainer(
+        hypernet,
+        input_embedder,
+        partial(training_step, lamda=config.lamda),
+        train_loader,
+        val_loader,
+        lr=lr_schedule,
+        epoch=first_epoch,
+    )
+
+    print("Validation before training:")
+    print()
+
+    trainer.validate(hypernet, input_embedder)
+
+    for _ in trange(config.epochs):
+        if "lr_schedule" in vars():
+            tqdm.write(f"learning rate: {trainer.learning_rate:.1e}")
+
+            if wandb.run is not None:
+                wandb.run.log(
+                    {
+                        "epoch": trainer.epoch,
+                        "learning_rate": trainer.learning_rate,
+                    }
+                )
+
+        hypernet, input_embedder, aux = trainer.train(hypernet, input_embedder)
+
+        if wandb.run is not None:
+            wandb.run.log(
+                {
+                    "epoch": trainer.epoch,
+                    "loss/train/mean": np.mean(aux["loss"]),
+                    "loss/train/std": np.std(aux["loss"]),
+                    "reg_loss/train/mean": np.mean(aux["reg_loss"]),
+                    "reg_loss/train/std": np.std(aux["reg_loss"]),
+                }
+            )
+        else:
+            tqdm.write(f"Loss:      {np.mean(aux['loss']):.3}")
+            tqdm.write(f"Reg. Loss: {np.mean(aux['reg_loss']):.3}")
+            tqdm.write("")
+
+        trainer.validate(hypernet, input_embedder)
+
+    model_path = Path(f"./models/{model_name}.safetensors")
+
+    model_path.parent.mkdir(exist_ok=True)
+
+    save_with_config_safetensors(model_path, OmegaConf.to_object(config), hypernet)
+
+    if wandb.run is not None:
+        model_artifact = wandb.Artifact(model_name, type="model")
+
+        model_artifact.add_file(str(model_path.with_suffix(".json")))
+        model_artifact.add_file(str(model_path.with_suffix(".safetensors")))
+
+        wandb.run.log_artifact(model_artifact)
+
+    print()
+    print()
+
+    trainer.make_plots(
+        hypernet, input_embedder, test_loader, image_folder=Path(f"./images/{model_name}")
+    )
+
+    if not args.no_umap:
+        umap_datasets = [dataset for dataset in train_loader.datasets]
+
+        if test_loader is not None:
+            assert isinstance(test_loader.dataset, Dataset)
+
+            umap_datasets.append(test_loader.dataset)
+
+        trainer.make_umap(
+            input_embedder, umap_datasets, image_folder=Path(f"./images/{model_name}")
+        )
+
+
+if __name__ == "__main__":
+    main()
