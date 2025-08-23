e5e180483c60e0784f94ac48b3d297f0c63ee4dd
Moritz Gmeiner <moritz@gmeiner.net>
2025-08-23 16:27:19 +0200

    default learning rate: 5e-5

--- hyper_lap/training/utils.py
+++ hyper_lap/training/utils.py
@@ -42,7 +42,7 @@ COMMON_CONFIG = {
     "batch_size": MISSING,
     "grad_accu": 1,
     "optim": {
-        "lr": MISSING,
+        "lr": 5e-4,
         "scheduler": "cosine",
         "epochs": "${epochs}",
         "grad_clip": 1.0,
