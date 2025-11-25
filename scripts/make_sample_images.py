237e57b24967db875cd1f01c4027d40468042728
Moritz Gmeiner <moritz@gmeiner.net>
2025-11-25 17:35:37 +0100

    load_sample_images: use load_model, fixed batch loading logic

--- scripts/make_sample_images.py
+++ scripts/make_sample_images.py
@@ -1,62 +1,18 @@
 import argparse
+import math
+import shutil
 from pathlib import Path
 
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-import jax.random as jr
 import jax.tree as jt
 import matplotlib.pyplot as plt
-import wandb
-from omegaconf import DictConfig
+from tqdm import trange
 from wandb.apis.public import Run
 
-from hyper_lap.embedder import InputEmbedder
-from hyper_lap.models import FilmUnet, HyperNet, Unet
-from hyper_lap.serialisation import load_pytree
-from hyper_lap.training.utils import get_datasets, load_model_artifact, print_config
-
-
-def load_unet(config: DictConfig, weights_path: Path) -> Unet:
-    unet = Unet(**config.unet, key=jr.PRNGKey(config.seed))
-
-    unet, _ = load_pytree(weights_path, (unet, None))
-
-    return unet
-
-
-def load_hypernet(
-    config: DictConfig, weights_path: Path, num_datasets: int
-) -> tuple[HyperNet, InputEmbedder]:
-    key = jr.PRNGKey(config.seed)
-
-    unet_key, hypernet_key, embedder_key = jr.split(key, 3)
-
-    unet = Unet(**config.unet, key=unet_key)
-
-    hypernet = HyperNet(unet, **config.hypernet, res=False, key=hypernet_key)
-
-    embedder = InputEmbedder(num_datasets=num_datasets, **config.embedder, key=embedder_key)
-
-    hypernet, embedder = load_pytree(weights_path, (hypernet, embedder))
-
-    return hypernet, embedder
-
-
-def load_film_unet(
-    config: DictConfig, weights_path: Path, num_datasets: int
-) -> tuple[FilmUnet, InputEmbedder]:
-    key = jr.PRNGKey(config.seed)
-
-    unet_key, embedder_key = jr.split(key)
-
-    film_unet = FilmUnet(**config.film_unet, key=unet_key)
-
-    embedder = InputEmbedder(num_datasets=num_datasets, **config.embedder, key=embedder_key)
-
-    film_unet, embedder = load_pytree(weights_path, (film_unet, embedder))
-
-    return film_unet, embedder
+import wandb
+from hyper_lap.training.utils import get_datasets, load_model, print_config
 
 
 def main():
@@ -67,11 +23,23 @@ def main():
 
     parser.add_argument("num-images", type=int, default=10)
 
+    parser.add_argument("--seed", type=int, default=None)
+
+    parser.add_argument("--datasets", type=str, default=None)
+
     args = parser.parse_args()
 
-    # artifact_name = args.artifact_name
-    run_name = args.run_name
+    run_name: str = args.run_name
     num_images: int = getattr(args, "num-images")
+    seed: int | None = args.seed
+
+    if not num_images > 0:
+        raise ValueError("num_images must be greater than 0")
+
+    if args.datasets is not None:
+        datasets = set(args.datasets.split(","))
+    else:
+        datasets = None
 
     api = wandb.Api()
 
@@ -79,75 +47,87 @@ def main():
 
     image_folder = Path(f"images/{run.name}")
 
-    image_folder.mkdir(parents=True, exist_ok=True)
+    if image_folder.exists():
+        shutil.rmtree(image_folder)
 
-    # artifact = api.artifact(artifact_name)
+    image_folder.mkdir(parents=True, exist_ok=False)
 
-    artifacts = [artifact for artifact in run.logged_artifacts() if artifact.type == "model"]
-
-    if len(artifacts) == 0:
-        raise RuntimeError('Cannot find "model" artifact for Run')
-    elif len(artifacts) > 1:
-        raise RuntimeError('Multiple "model" artifacts associated with Run')
-
-    artifact = artifacts[0]
-
-    config, weights_path = load_model_artifact(artifact.qualified_name)
+    config, net, embedder = load_model(run_name)
 
     print_config(config)
 
-    trainsets, valsets, oodsets = get_datasets(
+    print()
+    print(f"image folder: {image_folder}")
+    print()
+
+    _trainsets, valsets, oodsets = get_datasets(
         config.dataset,
         config.trainsets.split(","),
         config.oodsets.split(","),
-        batch_size=num_images,
+        batch_size=config.batch_size,
     )
 
-    num_datasets = len(trainsets) + len(oodsets)
+    for dataset in valsets + oodsets:
+        batch_size: int = dataset[0]["image"].shape[0]  # pyright: ignore
 
-    if "unet" in run.tags:
-        net = load_unet(config, weights_path)
-        embedder = None
-    elif "hypernet" in run.tags:
-        net, embedder = load_hypernet(config, weights_path, num_datasets)
-    elif "film_unet" in run.tags:
-        net, embedder = load_film_unet(config, weights_path, num_datasets)
-    else:
-        raise RuntimeError("Don't know how to load model")
+        if seed is not None:
+            dataset = dataset.seed(seed).shuffle()
 
-    for valset in valsets:
-        batch: dict = jt.map(lambda x: jnp.asarray(x) if eqx.is_array(x) else x, valset[0])
+        dataset_name: str = dataset[0]["name"]  # pyright: ignore
 
-        dataset_name = batch.pop("name")
+        if datasets is not None and dataset_name not in datasets:
+            continue
 
-        images = batch["image"][:num_images]
-        labels = batch["label"][:num_images]
+        subfolder = image_folder / dataset_name
 
-        if embedder is not None:
-            example_image = batch["example_image"]
-            example_label = batch["example_label"]
-            dataset_idx = batch["dataset_idx"]
+        subfolder.mkdir(exist_ok=True)
 
-            input_emb = jax.jit(embedder)(example_image, example_label, dataset_idx)
-        else:
-            input_emb = None
+        pbar = trange(num_images, desc=f"{dataset_name:<20}", leave=True)
 
-        logits = jax.jit(jax.vmap(net, in_axes=(0, None)))(images, input_emb)
+        pbar_it = iter(pbar)
 
-        preds = jnp.argmax(logits, axis=1)
+        try:
+            for batch_num in range(math.ceil(num_images / batch_size)):
+                batch: dict = jt.map(
+                    lambda x: jnp.asarray(x) if eqx.is_array(x) else x, dataset[batch_num]
+                )
 
-        for i in range(num_images):
-            fig, axs = plt.subplots(ncols=3, figsize=(12, 6))
+                assert batch_size == batch["image"].shape[0]
 
-            axs[0].imshow(images[i].mean(axis=0), cmap="gray")
-            axs[1].imshow(labels[i], cmap="gray")
-            axs[2].imshow(preds[i], cmap="gray")
+                if embedder is not None:
+                    example_image = batch["example_image"]
+                    example_label = batch["example_label"]
+                    dataset_idx = batch["dataset_idx"]
 
-            for ax in axs:
-                ax.axis("off")
+                    input_emb = eqx.filter_jit(embedder)(example_image, example_label, dataset_idx)
+                else:
+                    input_emb = None
 
-            fig.savefig(image_folder / f"{dataset_name}_{i:03}.pdf")
-            fig.savefig(image_folder / f"{dataset_name}_{i:03}.png")
+                for k in range(batch_size // 32):
+                    images = batch["image"][k * 32 : (k + 1) * 32]
+                    labels = batch["label"][k * 32 : (k + 1) * 32]
+
+                    logits = jax.jit(jax.vmap(net, in_axes=(0, None)))(images, input_emb)
+
+                    preds = jnp.argmax(logits, axis=1)
+
+                    for i in range(32):
+                        image_idx = next(pbar_it)
+
+                        fig, axs = plt.subplots(ncols=3, figsize=(12, 6))
+
+                        axs[0].imshow(images[i].mean(axis=0), cmap="gray")
+                        axs[1].imshow(labels[i], cmap="gray")
+                        axs[2].imshow(preds[i], cmap="gray")
+
+                        for ax in axs:
+                            ax.axis("off")
+
+                        fig.savefig(subfolder / f"{image_idx:03}.png")
+
+                        plt.close(fig)
+        except StopIteration:
+            break
 
 
 if __name__ == "__main__":
